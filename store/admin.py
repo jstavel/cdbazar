88c209a2b12299e143ae1f3a386aabf3cd7e2a0b
Jan Stavěl <stavel.jan@gmail.com>
2015-08-11 11:31:52 +0200

    doplnil jsem admin pro media type.

--- store/admin.py
+++ store/admin.py
@@ -5,9 +5,11 @@ class ItemAdmin(admin.ModelAdmin):
     list_display = ('article','commentary','barcode','packnumber','price', 'state_name','last_modified','to_store','home_page')
     list_filter = ('state','last_modified')
     pass
-
 admin.site.register(Item, ItemAdmin)
 
+class MediaAdmin(admin.ModelAdmin):
+    list_display = ("name","desc","order")
+admin.site.register(MediaType, MediaAdmin)
+
 admin.site.register(Article)
-admin.site.register(MediaType)
 admin.site.register(Picture)
