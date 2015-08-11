0e4ce4c2e4c2ed678df3e8deae4971b39f95ee49
Jan Stavel <stavel.jan@gmail.com>
2015-08-11 11:13:05 +0200

    added last modified

---
+++ store/migrations/0007_auto__add_field_article_last_modified.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Article.last_modified'
+        db.add_column(u'store_article', 'last_modified',
+                      self.gf('django.db.models.fields.DateTimeField')(auto_now=True, default=datetime.datetime(2015, 7, 15, 0, 0), blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Article.last_modified'
+        db.delete_column(u'store_article', 'last_modified')
+
+
+    models = {
+        u'store.article': {
+            'Meta': {'object_name': 'Article'},
+            'barcode': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '32', 'null': 'True', 'blank': 'True'}),
+            'discount': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'eshop': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'interpret': ('django.db.models.fields.CharField', [], {'max_length': '64', 'null': 'True', 'blank': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'mediaType': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['store.MediaType']", 'null': 'True', 'blank': 'True'}),
+            'origPrice': ('django.db.models.fields.DecimalField', [], {'default': '0.0', 'max_digits': '7', 'decimal_places': '2', 'blank': 'True'}),
+            'picture': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['store.Picture']", 'null': 'True', 'blank': 'True'}),
+            'pictureSource': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'publisher': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '126', 'null': 'True', 'blank': 'True'}),
+            'specification': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '222'}),
+            'to_store': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'tracklist': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'year': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
+        },
+        u'store.item': {
+            'Meta': {'object_name': 'Item'},
+            'article': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['store.Article']"}),
+            'barcode': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '128', 'blank': 'True'}),
+            'commentary': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'}),
+            'home_page': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'packnumber': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '128', 'blank': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'default': '0.0', 'max_digits': '7', 'decimal_places': '2', 'blank': 'True'}),
+            'state': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '1'}),
+            'to_store': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'})
+        },
+        u'store.itemaction': {
+            'Meta': {'ordering': "('-modified', '-created')", 'object_name': 'ItemAction'},
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['store.Item']"}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'blank': 'True'})
+        },
+        u'store.mediatype': {
+            'Meta': {'object_name': 'MediaType'},
+            'desc': ('django.db.models.fields.CharField', [], {'max_length': '48', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '16', 'db_index': 'True'})
+        },
+        u'store.picture': {
+            'Meta': {'object_name': 'Picture'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'img': ('django.db.models.fields.files.ImageField', [], {'max_length': "'2048'"})
+        }
+    }
+
+    complete_apps = ['store']
