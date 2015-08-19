8c1ee29fe704348fdd101c59877c65e5e2d706c4
Jan Stavěl <stavel.jan@gmail.com>
2015-08-19 22:14:26 +0200

    doplnil jsem modalni detail na vsech urovnich eshopu.

--- templates/eshop/article_list/new_articles_banner.js
+++ templates/eshop/article_list/new_articles_banner.js
@@ -1,1 +1,15 @@
-//
+$("li.new-article").click(function(){
+        var article_detail_href = $(this).find('a.article-link').attr('href');
+        $.ajax({
+		type: 'GET',
+		url: article_detail_href,
+		success: function(data){
+			$('.article_detail .modal-body').html(data['eshop/article_detail/detail.html']);
+			$('.article_detail').modal().on('shown',function(){
+			        eval(data['eshop/article_detail/js.js']);
+			        eval(data['eshop/basket/js.js']);
+                        });
+		},
+        });
+        return false;
+});
--- templates/eshop/article_list/tradeaction_banner.js
+++ templates/eshop/article_list/tradeaction_banner.js
@@ -1,1 +1,15 @@
-//
+$("li.tradeaction a.article-link").click(function(){
+        var article_detail_href = $(this).attr('href');
+        $.ajax({
+		type: 'GET',
+		url: article_detail_href,
+		success: function(data){
+			$('.article_detail .modal-body').html(data['eshop/article_detail/detail.html']);
+			$('.article_detail').modal().on('shown',function(){
+			        eval(data['eshop/article_detail/js.js']);
+			        eval(data['eshop/basket/js.js']);
+                        });
+		},
+        });
+        return false;
+});
